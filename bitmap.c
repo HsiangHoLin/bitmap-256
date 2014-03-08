1dabd6f27b2c8b99438ea7dea811ee1c01a182cd
HsiangHoLin <mymaydayya@gmail.com>
2014-03-08 11:53:22 +0800

    First Commit

---
+++ bitmap.c
@@ -0,0 +1,54 @@
+#include "bitmap.h"
+#include "stdlib.h"
+
+#define BITMASK 0X1F
+#define BITSHIFT 5
+#define BLOCK 8		/* 3 bits = 2^3 = 8 blocks */
+#define WORDSIZE 32
+
+struct BITMAP_STRUCT 
+{
+	unsigned int bitmap[BLOCK];
+	/* 8 * 32 = 256 bits */
+};
+
+bitmap_struct* bitmap_new(void)
+{
+	return (bitmap_struct*) malloc (sizeof(bitmap_struct));
+}
+
+void bitmap_free(bitmap_struct *bms)
+{
+	free(bms);
+}
+
+void bitmap_clear(bitmap_struct* bms)
+{
+	unsigned int i;
+	for(i = 0; i < BLOCK; i++)
+		bms->bitmap[i] = 0;
+}
+
+void bitmap_print(bitmap_struct* bms)
+{
+	unsigned int i;
+	for(i = 0; i < BLOCK; i++)
+		printf("%d: %0x\n", i, bms->bitmap[i]);
+}
+
+int bitmap_set(bitmap_struct* bms, unsigned int val)
+{
+	if(val >= BLOCK*WORDSIZE)
+		return -1;
+	bms->bitmap[(val >> BITSHIFT)] |= (1 << (val & BITMASK));
+    return 0;
+}
+
+int bitmap_check(bitmap_struct* bms, unsigned int val)
+{
+	if(val >= BLOCK*WORDSIZE)
+		return 0;
+	if(bms->bitmap[(val >> BITSHIFT)] & (1 << (val & BITMASK)))
+		return 1;
+	return 0;
+}
---
+++ bitmap.h
@@ -0,0 +1,14 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include <stdio.h>
+
+typedef struct BITMAP_STRUCT bitmap_struct;
+
+bitmap_struct* bitmap_new(void);
+void bitmap_free(bitmap_struct *bms);
+void bitmap_clear(bitmap_struct* bms);
+void bitmap_print(bitmap_struct* bms);
+int bitmap_set(bitmap_struct* bms, unsigned int val);
+int bitmap_check(bitmap_struct* bms, unsigned int val);
+#endif
